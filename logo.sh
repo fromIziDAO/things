28049c3b3bf1160e45ef3da69b192030048c92b6
wladie <wladie.miller.dodo@gmail.com>
2023-03-03 00:58:34 +0300

    upd

---
+++ logo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function colors {
+YELLOW="\e[33m"
+BLACK="\e[30m"
+ENDCOLOR="\e[0m"
+}
+
+echo -e "${YELLOW}######                                           #                                               ${ENDCOLOR}"
+echo -e "${YELLOW}#     #   ##   #    #   ##   #    #   ##        # #   #      #      #   ##   #    #  ####  ######${ENDCOLOR}"
+echo -e "${YELLOW}#     #  #  #  ##   #  #  #  ##   #  #  #      #   #  #      #      #  #  #  ##   # #    # #     ${ENDCOLOR}"
+echo -e "${YELLOW}######  #    # # #  # #    # # #  # #    #    #     # #      #      # #    # # #  # #      ##### ${ENDCOLOR}"
+echo -e "${YELLOW}#     # ###### #  # # ###### #  # # ######    ####### #      #      # ###### #  # # #      #     ${ENDCOLOR}"
+echo -e "${YELLOW}#     # #    # #   ## #    # #   ## #    #    #     # #      #      # #    # #   ## #    # #     ${ENDCOLOR}"
+echo -e "${YELLOW}######  #    # #    # #    # #    # #    #    #     # ###### ###### # #    # #    #  ####  ######${ENDCOLOR}"
