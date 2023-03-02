46fb10bd05b62a976ab0efbb2cd450318c399e09
wladie <wladie.miller.dodo@gmail.com>
2023-03-03 02:35:13 +0300

    upd

--- logo.sh
+++ logo.sh
@@ -5,7 +5,9 @@ YELLOW="\e[33m"
 BLACK="\e[30m"
 ENDCOLOR="\e[0m"
 }
-echo -e "${YELLOW}                                        ${ENDCOLOR}"
+
+colors
+
 echo -e "${YELLOW}██╗███████╗██╗  ██████╗░░█████╗░░█████╗░${ENDCOLOR}"
 echo -e "${YELLOW}██║╚════██║██║  ██╔══██╗██╔══██╗██╔══██╗${ENDCOLOR}"
 echo -e "${YELLOW}██║░░███╔═╝██║  ██║░░██║███████║██║░░██║${ENDCOLOR}"
